86d955f93010527c3409dd900d22e01a750ba18c
Kaushik Bhowmick <56549069+kaushik2107-bit@users.noreply.github.com>
2025-03-07 20:14:04 +0530

    fix: database connection

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,9 +1,10 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "postgres",
-    port: 54322
-})
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+});
